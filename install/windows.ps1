3b375fe00ea1dc6a64036ec0e1aed3636151557a
Albert Liu <albertymliu@gmail.com>
2019-09-23 23:35:24 -0400

    added more windows stuffs

--- install/windows.ps1
+++ install/windows.ps1
@@ -1,6 +1,12 @@
+Set-ExecutionPolicy RemoteSigned
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-New-Item -ItemType SymbolicLink -Path ".\.gitconfig" -Target ".\code\config\programs\neovim"
-New-Item -ItemType SymbolicLink -Path ".\AppData\Local\nvim" -Target ".\code\config\programs\neovim"
+choco install git -params '"/GitAndUnixToolsOnPath"'
+choco install ripgrep fd
+
+
+# New-Item -ItemType SymbolicLink -Path ".\.gitconfig" -Target ".\code\config\programs\neovim"
+# New-Item -ItemType SymbolicLink -Path ".\AppData\Local\nvim" -Target ".\code\config\programs\neovim"
 
