af1a9d6bbd82753f4ea40dad7fc953b5825ffbf1
Albert Liu <albertymliu@gmail.com>
2020-03-19 09:54:09 -0400

    fixed problems with rm in windows install script

--- install/windows.ps1
+++ install/windows.ps1
@@ -5,29 +5,30 @@ $installDir = [System.IO.Path]::GetDirectoryName($PSCommandPath)
 $configDir = [System.IO.Path]::GetDirectoryName($installDir)
 
 
-iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
+# iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
 
-choco install git.install --params "/GitAndUnixToolsOnPath /WindowsTerminal" -y
+# choco install git.install --params "/GitAndUnixToolsOnPath /WindowsTerminal" -y
 choco install ripgrep fd -y
 
 # New-Item -ItemType SymbolicLink -Path ".\.gitconfig" -Target ".\code\config\programs\neovim"
 # New-Item -ItemType SymbolicLink -Path ".\AppData\Local\nvim" -Target ".\code\config\programs\neovim"
 
 # Install Windows Subsystem for Linux
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 # Install Ubuntu
 # Invoke-RestMethod -Uri "https://aka.ms/wsl-debian-gnulinux" -OutFile "~/Ubuntu.zip" -UseBasicParsing
 
 # Setting up Vim
+$(get-item "$HOME\.vimrc").Delete()
+$(get-item "$HOME\vimfiles").Delete()
 New-Item -ItemType SymbolicLink -Path "$HOME\.vimrc" -Target "$configDir\programs\neovim\init.vim"
 New-Item -ItemType SymbolicLink -Path "$HOME\vimfiles" -Target "$configDir\programs\neovim"
 
 # Making Windows Terminal behave correctly
 # TODO Fix this by using the same features that every other script uses
-rm -rf "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+$(get-item "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState").Delete()
 New-Item -ItemType SymbolicLink -Path "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -Target "$configDir\programs\windows-terminal"
 
-# Use mklink in command prompt to make a hard link to the correct place
 
 
