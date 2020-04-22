9c667cccae16df4556815afd32554a46dbc2bffc
Albert Liu <albertymliu@gmail.com>
2020-04-21 23:36:03 -0400

    added concatenation for tstring, figured out multithreading stuff

--- libs/cplusplus/string.cpp
+++ libs/cplusplus/string.cpp
@@ -1,19 +1,23 @@
 #include "string.h"
 #include <deque>
-#include <vector>
+#include <mutex>
 
-#define BLOCK_SIZE 1024 * 1024
+#define BLOCK_SIZE 1024 * 1024 * 4
+
+std::mutex mut;
 
 // Circular char queue with enqueue(uint64_t) and dequeue(uint64_t)
 struct CharQueue {
   char *begin, *end;
   char *section_begin, *section_end;
 
-  CharQueue() {
+  CharQueue() noexcept {
     begin = new char[BLOCK_SIZE];
     end = begin + BLOCK_SIZE;
     section_begin = section_end = begin;
   }
+
+  ~CharQueue() noexcept { delete begin; }
 
   CharQueue(uint64_t len) {
     begin = new char[len];
@@ -21,7 +25,8 @@ struct CharQueue {
     section_begin = section_end = begin;
   }
 
-  char *enqueue(uint64_t len) {
+  char *enqueue(uint64_t len) noexcept {
+    std::lock_guard<std::mutex> g(mut);
     if (section_begin <= section_end) {
       if (end - section_end >= len) {
         char *ret_val = section_end;
@@ -46,7 +51,7 @@ struct CharQueue {
     return nullptr;
   }
 
-  void dequeue(uint64_t len) {
+  void dequeue(uint64_t len) noexcept {
     if (section_begin <= section_end) {
       section_begin += len;
       if (section_begin == section_end) {
@@ -74,17 +79,32 @@ struct CharQueue {
 
 struct StringTracker {
   char *begin, *end;
-  uint64_t ref_count = 1;
+  uint64_t ref_count;
+  StringTracker(char *begin_, char *end_) noexcept
+      : begin(begin_), end(end_), ref_count(1) {}
 };
 
 uint64_t base_idx = 1;
+CharQueue pool;
 std::deque<StringTracker> tracker_queue;
-std::vector<StringBlock> blocks;
+
+TString::TString() noexcept : begin(nullptr), end(nullptr), tracker_index(0) {}
 
 TString::TString(const char *s) noexcept {
-  begin = s;
-  end = begin + strlen(s);
-  tracker_index = 0;
+  uint64_t len = strlen(s);
+  if ((begin = pool.enqueue(len)) == nullptr) {
+    begin = new char[len];
+  }
+  end = begin + len;
+
+  for (char *i = begin; *s != '\0'; i++, s++)
+    *i = *s;
+
+  {
+    std::lock_guard<std::mutex> g(mut);
+    tracker_index = base_idx + tracker_queue.size();
+    tracker_queue.emplace_back(begin, end);
+  }
 }
 
 TString::TString(TString &&other) noexcept
@@ -96,19 +116,57 @@ TString::TString(TString &&other) noexcept
 
 TString::TString(const TString &other) noexcept
     : begin(other.begin), end(other.end), tracker_index(other.tracker_index) {
+  std::lock_guard<std::mutex> g(mut);
   tracker_queue[other.tracker_index - base_idx].ref_count++;
 }
 
 TString::~TString() noexcept {
-  if (tracker_index > 0) {
+  if (tracker_index == -1) {
+    delete begin;
+  } else if (tracker_index != 0) {
+    std::lock_guard<std::mutex> g(mut);
     tracker_queue[tracker_index - base_idx].ref_count--;
-  } else if (tracker_index < 0) {
+    for (StringTracker tracker = tracker_queue.front(); tracker.ref_count == 0;
+         tracker_queue.pop_front(), tracker = tracker_queue.front()) {
+      pool.dequeue(tracker.end - tracker.begin);
+    }
   }
 }
 
-std::ostream &operator<<(std::ostream &os, const TString &tstring) noexcept {
-  for (const char *i = tstring.begin; i != tstring.end; i++) {
-    os << *i;
+TString &TString::operator=(const TString &other) noexcept {
+  begin = other.begin;
+  end = other.end;
+  tracker_index = other.tracker_index;
+
+  std::lock_guard<std::mutex> g(mut);
+  tracker_queue[tracker_index - base_idx].ref_count++;
+  return *this;
+}
+
+TString operator+(const TString &a, const TString &b) noexcept {
+  TString t;
+  uint64_t len = (a.end - a.begin) + (b.end - b.begin);
+  if ((t.begin = pool.enqueue(len)) == nullptr) {
+    t.begin = new char[len];
   }
+  t.end = t.begin + len;
+
+  char *dest = t.begin, *src;
+  for (src = a.begin; src != a.end; dest++, src++)
+    *dest = *src;
+  for (src = b.begin; src != b.end; dest++, src++)
+    *dest = *src;
+
+  {
+    std::lock_guard<std::mutex> g(mut);
+    t.tracker_index = base_idx + tracker_queue.size();
+    tracker_queue.emplace_back(t.begin, t.end);
+  }
+  return t;
+}
+
+std::ostream &operator<<(std::ostream &os, const TString &tstring) noexcept {
+  for (const char *i = tstring.begin; i != tstring.end; i++)
+    os << *i;
   return os;
 }
--- libs/cplusplus/string.h
+++ libs/cplusplus/string.h
@@ -3,12 +3,17 @@
 #include <ostream>
 
 struct TString {
-  const char *begin = nullptr, *end = nullptr;
-  int64_t tracker_index = 0;
+  char *begin = nullptr, *end = nullptr;
+  uint64_t tracker_index = 0;
 
+  TString() noexcept;
   TString(const char *) noexcept;
   TString(TString &&other) noexcept;
   TString(const TString &other) noexcept;
   ~TString() noexcept;
+
+  TString &operator=(const TString &) noexcept;
 };
+
+TString operator+(const TString &, const TString &) noexcept;
 std::ostream &operator<<(std::ostream &, const TString &) noexcept;
