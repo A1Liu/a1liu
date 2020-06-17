0759d5de3d74907f30afd141d814724813e10ab6
Albert Liu <albertymliu@gmail.com>
2020-06-17 18:35:08 -0400

    added stuff for all the primitive types

--- libs/go/main.go
+++ libs/go/main.go
@@ -4,14 +4,25 @@ import (
 	"goPlaceholder/utils"
 )
 
+type Hello struct {
+	Hello string
+}
+
+func (h *Hello) ParseFlag(hello string) error {
+	utils.Print("hi\n")
+	h.Hello = "what"
+	return nil
+}
+
 type Parser struct {
-	Name *string
+	Name       Hello
+	Occupation *string
 }
 
 func main() {
 	var parser Parser
-	utils.Print("%v\n", utils.ArgParse(&parser, "-Name", "hello"))
+	utils.Print("%v\n", utils.ArgParse(&parser, "-Name", "hello", "-Occupation", "goodbye"))
 
-	utils.Print("%v\n", *parser.Name)
-
+	utils.Print("%v\n", parser.Name)
+	utils.Print("%v\n", *parser.Occupation)
 }
