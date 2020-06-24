4af9e671d144295c1ec0eb48e18db2433cb78b9b
Albert Liu <albertymliu@gmail.com>
2020-06-24 15:02:26 -0400

    cleaning, documenting

--- libs/c/debug_allocator.h
+++ libs/c/debug_allocator.h
@@ -5,7 +5,14 @@
 #define free(ptr) __debug_dealloc(ptr, __FILE__, __LINE__)
 #define check(ptr) (__debug_check_alloc(ptr, __FILE__, __LINE__), ptr)
 
+// Replacement for malloc that tracks the file & line where it was called
 void *__debug_alloc(size_t, char *, unsigned int);
+
+// Replacement for realloc that tracks the file & line where it was called
 void *__debug_realloc(void *, size_t, char *, unsigned int);
+
+// Replacement for free that tracks the file & line where it was called
 void __debug_dealloc(void *, char *, unsigned int);
+
+// Checks the given pointer to see if it has been allocated already or not
 void __debug_check_alloc(void *, char *, unsigned int);
