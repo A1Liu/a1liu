22c8bb53ec719ee46ef7a236b2492c230bf5c59f
Albert Liu <albertymliu@gmail.com>
2020-06-27 23:57:50 -0400

    added exports of string functions

--- libs/c/strings.h
+++ libs/c/strings.h
@@ -4,3 +4,13 @@ typedef struct {
   char *begin;
   uint64_t len;
 } String;
+
+typedef char bool;
+
+String string_new(char *);
+
+String string_from_parts(char *, uint64_t);
+
+bool string_equals(String, String);
+
+bool string_equals_str(String, char *);
