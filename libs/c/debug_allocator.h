9b84699be2e932b83fd82ab0f58d78649cf7a812
Albert Liu <albertymliu@gmail.com>
2020-07-18 01:16:37 -0400

    updating things, fixing re-include bug in C debug_allocator

--- libs/c/debug_allocator.h
+++ libs/c/debug_allocator.h
@@ -1,17 +1,32 @@
+#ifndef DEBUG_ALLOCATOR
+#define DEBUG_ALLOCATOR
+
 #include <stddef.h>
 
 // malloc fills its memory with this value before handing you a pointer to it
 #define DEBUG_UNINIT 0xdadfaded
 // realloc and free both set freed memory to this value before returning
 #define DEBUG_FREED 0xdeadbeef
-// The buffer memory of freed blocks gets set to this value by realloc and free
-#define DEBUG_NEARBY_FREED 0xbadadded
 // The buffer memory of allocated blocks gets set to this value by malloc
 #define DEBUG_NEARBY_ALLOCATED 0xaabcdeff
+// The buffer memory of freed blocks gets set to this value by realloc and free
+#define DEBUG_NEARBY_FREED 0xbadadded
 
+#ifdef malloc
+#undef malloc
 #define malloc(size) __debug_alloc(size, __FILE__, __LINE__)
+#endif
+
+#ifdef realloc
+#undef realloc
 #define realloc(ptr, size) __debug_realloc(ptr, size, __FILE__, __LINE__)
+#endif
+
+#ifdef free
+#undef free
 #define free(ptr) __debug_dealloc(ptr, __FILE__, __LINE__)
+#endif
+
 #define check(ptr) (__debug_check_alloc(ptr, __FILE__, __LINE__), ptr)
 
 // Replacement for malloc that tracks the file & line where it was called
@@ -25,3 +40,5 @@ void __debug_dealloc(void *, char *, unsigned int);
 
 // Checks the given pointer to see if it has been allocated already or not
 void __debug_check_alloc(void *, char *, unsigned int);
+
+#endif
