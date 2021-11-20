f4bf9219e842d6bc2e3d405446210f84d2ad255b
Albert Liu <albertymliu@gmail.com>
2021-11-20 13:48:27 -0500

    fixed up a lil boi'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/config/' : '',
+  basePath: isProd ? '/config/' : '',
   reactStrictMode: true,
 };
