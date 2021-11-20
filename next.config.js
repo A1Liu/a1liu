6c3d968f8d6772f6d1ec6f79962800c14fe6e5f6
Albert Liu <albertymliu@gmail.com>
2021-11-20 13:49:26 -0500

    duh

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '/config/' : '',
+  basePath: isProd ? '/config' : '',
   reactStrictMode: true,
 };
