556f61d89f5e00bc155a3169b96d1518e1e555b2
Albert Liu <albertymliu@gmail.com>
2021-11-26 22:38:59 -0500

    spoop spoop spoop

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,22 +1,29 @@
 import Layout from "../../components/Layout";
 import { dir } from "../../components/util";
-// import { readdir } from "fs/promises";
+import { readdir } from "fs/promises";
 
+export async function getStaticProps({ params }) {
+  const allFiles = await readdir("./pages/docs");
+  const files = allFiles.filter((f) => f !== "index.tsx");
 
-// export async function getStaticProps({ params }) {
-  // const allFiles = await readdir("./pages/docs");
-  // const files = allFiles.filter(f => f !== "index.tsx");
-  // console.log(files);
-
-  // const props = { files: [] };
-  // return { props };
-// }
-
+  const props = { files };
+  return { props };
+}
 
 const Documents = ({ files }) => {
+  const urls = files.map((f) => `/docs/${f.replace(/\.[^/.]+$/, "")}`);
+
   return (
     <Layout>
       <h1>Documents</h1>
+      {urls.map((url) => {
+        return (
+          <>
+            <a href={url}>{url}</a>
+            <br />
+          </>
+        );
+      })}
     </Layout>
   );
 };
