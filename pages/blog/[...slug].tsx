fb4d2fbb006371645fff82f46f7c444ae0bf1283
Albert Liu <albertymliu@gmail.com>
2021-12-06 02:16:42 -0500

    idk its in progress ig

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import React from "react";
+
+export async function getStaticPaths() {
+  const params = { params: { slug: ["2020", "4"] } };
+
+  return {
+    paths: [params],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+}
 
 const BlogPage: React.VFC = () => {
   return <div></div>;
