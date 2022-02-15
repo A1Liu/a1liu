c07a419e622763dc394e1a487335abfaf39e116d
Albert Liu <albertymliu@gmail.com>
2022-02-14 23:01:51 -0500

    idk man

--- config/install/linux_mint.sh
+++ config/install/linux_mint.sh
@@ -45,3 +45,6 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
+
