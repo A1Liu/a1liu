ead5158cbaaf2e91ed00d52bf740a16c480b5917
Albert Liu <albertymliu@gmail.com>
2022-05-02 18:07:16 -0400

    updated script

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,11 +1,15 @@
-git checkout main
-git branch -D production || true
-git checkout -b production
-git checkout production
+git checkout main || exit 1
+git push || exit 1
+git checkout -b production || exit 1
 
 echo '!*.wasm' >> public/.gitignore
 
 zig build -Drelease-small || exit 1
 cp zig-out/lib/*.wasm public/assets
 
+git add .
+git commit -m "Deployment"
+
 git push -uf origin production
+git checkout main
+git branch -D production
