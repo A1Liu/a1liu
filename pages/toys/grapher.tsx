d84090b25ab5ec13fb6f5d8093f6438754be7213
Albert Liu <albertymliu@gmail.com>
2022-05-03 15:01:25 -0400

    meh

--- pages/toys/grapher.tsx
+++ pages/toys/grapher.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import type { Dispatch, SetStateAction } from "react";
-import css from "./kilordle.module.css";
+import css from "./grapher.module.css";
 import * as wasm from "components/wasm";
+import { useToast } from "components/errors";
 import cx from "classnames";
 import create from "zustand";
 
+type WebGl = WebGL2RenderingContext;
+
 interface GrapherCb {
-  setWasmRef: (wasmRef: wasm.Ref) => void;
+  initWasm: (wasmRef: wasm.Ref) => void;
+  initGl: (gl: WebGL) => void;
 }
 
 interface GrapherState {
-  wasmRef: wasm.Ref | undefined;
+  gl: WebGL | null;
+  wasmRef: wasm.Ref | null;
   callbacks: GrapherCb;
 }
 
 const useStore = create<GrapherState>((set, get) => {
-  const setWasmRef = (wasmRef: wasm.Ref) => set({ wasmRef });
-
+  const initWasm = (wasmRef: wasm.Ref) => set({ wasmRef });
+  const initGl = (gl: WebGL) => set({ gl });
   return {
-    wasmRef: undefined,
+    glContext: null,
+    wasmRef: null,
     callbacks: {
-      setWasmRef,
+      initWasm,
+      initGl,
     },
   };
 });
@@ -29,6 +36,19 @@ const Grapher: React.VFC = () => {
   const cb = useStore((state) => state.callbacks);
   const wasmRef = useStore((state) => state.wasmRef);
   const [text, setText] = React.useState("");
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const toast = useToast();
+
+  React.useEffect(() => {
+    const gl = canvasRef.current.getContext("webgl2");
+    if (!gl) {
+      toast.add("error", null, "WebGL2 not supported!");
+    } else {
+      toast.add("success", null, "WebGL2 context initialized!");
+    }
+
+    cb.initGl(gl);
+  }, [canvasRef, toast, cb]);
 
   React.useEffect(() => {
     const wasmPromise = wasm.fetchWasm("/assets/grapher.wasm", {
@@ -39,31 +59,35 @@ const Grapher: React.VFC = () => {
 
     wasmPromise.then((ref) => {
       ref.abi.init();
-      cb.setWasmRef(ref);
+      cb.initWasm(ref);
     });
   }, [cb]);
 
   return (
-    <form
-      onSubmit={(evt) => {
-        evt.preventDefault();
+    <div className={css.wrapper}>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
 
-        if (!wasmRef) return;
+          if (!wasmRef) return;
 
-        const idx = wasmRef.addObj(text);
-        wasmRef.abi.print(idx);
-      }}
-    >
-      <label>
-        Name:
-        <input
-          type="text"
-          value={text}
-          onChange={(evt) => setText(evt.target.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+          const idx = wasmRef.addObj(text);
+          wasmRef.abi.print(idx);
+        }}
+      >
+        <label>
+          Name:
+          <input
+            type="text"
+            value={text}
+            onChange={(evt) => setText(evt.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <canvas ref={canvasRef} className={css.canvas} />
+    </div>
   );
 };
 
