aa744ada931d4f6bfc0c44117429ab04e0c88a19
Albert Liu <albertymliu@gmail.com>
2022-05-02 20:30:06 -0400

    meh

--- src/test.zig
+++ src/test.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 const liu = @import("liu");
-const kilo = @import("./kilordle.zig");
+
+const WordArray = packed struct {
+    a1: u6,
+    a2_1: u2,
+    a2_2: u4,
+    a3_1: u4,
+    a3_2: u2,
+    a4: u6,
+    a5: u6,
+};
 
 pub fn main() !void {
-    try kilo.init();
-
-    _ = try kilo.submitWord("HELLO".*);
-    _ = try kilo.submitWord("WORDS".*);
+    std.debug.print("{}\n", .{@sizeOf(WordArray)});
 }
