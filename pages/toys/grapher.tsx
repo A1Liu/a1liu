65e38e7ff51cfd15126c661eff45c49ee2ab6d52
Albert Liu <albertymliu@gmail.com>
2022-05-04 19:34:31 -0400

    org

--- pages/toys/grapher.tsx
+++ pages/toys/grapher.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { Dispatch, SetStateAction } from "react";
+import * as GL from "components/webgl";
+import type { WebGl } from "components/webgl";
 import css from "./grapher.module.css";
 import * as wasm from "components/wasm";
 import { useToast } from "components/errors";
@@ -9,23 +11,26 @@ import create from "zustand";
 // User sets image size/resolution manually
 // User customizes basically every part
 
-type WebGl = WebGL2RenderingContext;
-
 interface GrapherCb {
   initWasm: (wasmRef: wasm.Ref) => void;
-  initGl: (gl: WebGl, program: WebGLProgram) => void;
+  initGl: (gl: GrapherGl) => void;
+}
+
+interface GrapherGl {
+  ctx: WebGl;
+  program: WebGLProgram;
+  vao: WebGLVertexArrayObject;
 }
 
 interface GrapherState {
-  gl: WebGl | null;
-  program: WebGLProgram | null;
+  gl: GrapherGl | null;
   wasmRef: wasm.Ref | null;
   callbacks: GrapherCb;
 }
 
 const useStore = create<GrapherState>((set, get) => {
   const initWasm = (wasmRef: wasm.Ref) => set({ wasmRef });
-  const initGl = (gl: WebGl, program: WebGLProgram) => set({ gl, program });
+  const initGl = (gl: GrapherGl) => set({ gl });
 
   return {
     gl: null,
@@ -39,6 +44,88 @@ const useStore = create<GrapherState>((set, get) => {
   };
 });
 
+const initGl = async (
+  canvas: HTMLCanvasElement | null,
+  cb: GrapherCb
+): Promise<GrapherGl | null> => {
+  const ctx = canvas?.getContext("webgl2");
+  if (!ctx) {
+    return null;
+  }
+
+  const [vertSrc, fragSrc] = await Promise.all([
+    fetch("/assets/grapher.vert").then((r) => r.text()),
+    fetch("/assets/grapher.frag").then((r) => r.text()),
+  ]);
+
+  const vertexShader = GL.createShader(ctx, ctx.VERTEX_SHADER, vertSrc);
+  const fragmentShader = GL.createShader(ctx, ctx.FRAGMENT_SHADER, fragSrc);
+  if (!vertexShader || !fragmentShader) return null;
+
+  const program = GL.createProgram(ctx, vertexShader, fragmentShader);
+  if (!program) return null;
+
+  const vao = ctx.createVertexArray();
+  if (!vao) return null;
+
+  const posLocation = 0;
+
+  ctx.bindAttribLocation(program, posLocation, "pos");
+
+  const positionBuffer = ctx.createBuffer();
+  ctx.bindBuffer(ctx.ARRAY_BUFFER, positionBuffer);
+
+  // three 2d points
+  const positions = [0, 0, 0, 0.5, 0.7, 0];
+  ctx.bufferData(
+    ctx.ARRAY_BUFFER,
+    new Float32Array(positions),
+    ctx.STATIC_DRAW
+  );
+
+  ctx.bindVertexArray(vao);
+
+  ctx.enableVertexAttribArray(posLocation);
+
+  {
+    const size = 2; // 2 components per iteration
+    const type = ctx.FLOAT; // the data is 32bit floats
+    const normalize = false; // don't normalize the data
+    const stride = 0; // 0 = move forward size * sizeof(type)
+    const offset = 0; // start at the beginning of the buffer
+    ctx.vertexAttribPointer(posLocation, size, type, normalize, stride, offset);
+  }
+
+  ctx.bindVertexArray(null);
+
+  const ggl: GrapherGl = { ctx, program, vao };
+
+  return ggl;
+};
+
+const render = (ggl: GrapherGl) => {
+  const ctx = ggl.ctx;
+
+  GL.resizeCanvasToDisplaySize(ctx.canvas);
+
+  ctx.viewport(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  ctx.clearColor(0, 0, 0, 0);
+  ctx.clear(ctx.COLOR_BUFFER_BIT);
+
+  ctx.useProgram(ggl.program);
+
+  // Bind the attribute/buffer set we want.
+  ctx.bindVertexArray(ggl.vao);
+
+  {
+    const primitiveType = ctx.TRIANGLES;
+    const offset = 0;
+    const count = 3;
+    ctx.drawArrays(primitiveType, offset, count);
+  }
+};
+
 const Grapher: React.VFC = () => {
   const cb = useStore((state) => state.callbacks);
   const wasmRef = useStore((state) => state.wasmRef);
@@ -47,82 +134,17 @@ const Grapher: React.VFC = () => {
   const toast = useToast();
 
   React.useEffect(() => {
-    const gl = canvasRef.current?.getContext("webgl2");
-    if (!gl) {
-      toast.add("error", null, "WebGL2 not supported!");
-      return;
-    }
-
-    const init = async () => {
-      const [vertSrc, fragSrc] = await Promise.all([
-        fetch("/assets/grapher.vert").then((r) => r.text()),
-        fetch("/assets/grapher.frag").then((r) => r.text()),
-      ]);
-
-      const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertSrc);
-      const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragSrc);
-      if (!vertexShader || !fragmentShader) return;
-
-      const program = createProgram(gl, vertexShader, fragmentShader);
-      if (!program) return;
-
-      const posLocation = gl.getAttribLocation(program, "pos");
-      const positionBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-      // three 2d points
-      const positions = [0, 0, 0, 0.5, 0.7, 0];
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(positions),
-        gl.STATIC_DRAW
-      );
-
-      const vao = gl.createVertexArray();
-      gl.bindVertexArray(vao);
-
-      gl.enableVertexAttribArray(posLocation);
-
-      {
-        const size = 2; // 2 components per iteration
-        const type = gl.FLOAT; // the data is 32bit floats
-        const normalize = false; // don't normalize the data
-        const stride = 0; // 0 = move forward size * sizeof(type)
-        const offset = 0; // start at the beginning of the buffer
-        gl.vertexAttribPointer(
-          posLocation,
-          size,
-          type,
-          normalize,
-          stride,
-          offset
-        );
+    initGl(canvasRef.current, cb).then((ggl) => {
+      if (!ggl) {
+        toast.add("error", null, "WebGL2 not supported!");
+        return;
       }
 
-      resizeCanvasToDisplaySize(gl.canvas);
+      render(ggl);
+      cb.initGl(ggl);
 
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-      gl.clearColor(0, 0, 0, 0);
-      gl.clear(gl.COLOR_BUFFER_BIT);
-
-      gl.useProgram(program);
-
-      // Bind the attribute/buffer set we want.
-      gl.bindVertexArray(vao);
-
-      {
-        const primitiveType = gl.TRIANGLES;
-        const offset = 0;
-        const count = 3;
-        gl.drawArrays(primitiveType, offset, count);
-      }
-
-      cb.initGl(gl, program);
       toast.add("success", null, "WebGL2 context initialized!");
-    };
-
-    init();
+    });
   }, [canvasRef, toast, cb]);
 
   React.useEffect(() => {
@@ -167,62 +189,3 @@ const Grapher: React.VFC = () => {
 };
 
 export default Grapher;
-
-function createShader(
-  gl: WebGl,
-  shaderType: number,
-  source: string
-): WebGLShader | null {
-  const shader = gl.createShader(shaderType);
-  if (!shader) return null;
-
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
-
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-  return null;
-}
-
-function createProgram(
-  gl: WebGl,
-  vertexShader: WebGLShader,
-  fragmentShader: WebGLShader
-): WebGLProgram | null {
-  const program = gl.createProgram();
-  if (!program) return null;
-
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
-
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
-  return null;
-}
-
-function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement): boolean {
-  // Lookup the size the browser is displaying the canvas in CSS pixels.
-  const displayWidth = canvas.clientWidth;
-  const displayHeight = canvas.clientHeight;
-
-  // Check if the canvas is not the same size.
-  const needResize =
-    canvas.width !== displayWidth || canvas.height !== displayHeight;
-
-  if (needResize) {
-    // Make the canvas the same size
-    canvas.width = displayWidth;
-    canvas.height = displayHeight;
-  }
-
-  return needResize;
-}
--- public/assets/grapher.vert
+++ public/assets/grapher.vert
@@ -1,6 +1,6 @@
 #version 300 es
 
-layout(location = 0) in vec4 pos;
+in vec4 pos;
 
 void main() {
   gl_Position = pos;
