e48f405ababb87a79717e7430bcff7187d4042f6
Albert Liu <albertymliu@gmail.com>
2022-05-07 22:42:46 -0400

    added line stuffs

--- src/grapher.zig
+++ src/grapher.zig
@@ -14,7 +14,8 @@ const ext = struct {
     extern fn setTriangles(obj: wasm.Obj) void;
 
     fn onClickExt(posX: f32, posY: f32, width: f32, height: f32) callconv(.C) void {
-        onClick(posX, posY, width, height) catch @panic("onClick failed");
+        const pos = translatePos(posX, posY, .{ width, height });
+        onClick(pos) catch @panic("onClick failed");
     }
 
     fn printExt(msg: wasm.Obj) callconv(.C) void {
@@ -34,7 +35,8 @@ comptime {
 
 // First 36 triangles are reserved for the lines created during triangle drawing
 var triangles: ArrayList(f32) = undefined;
-var temp_triangle: std.BoundedArray(f32, 6) = undefined;
+var temp_type: enum { triangle, line } = .triangle;
+var temp_points: std.BoundedArray(f32, 6) = undefined;
 var temp_begin: usize = 0;
 
 fn translatePos(posX: f32, posY: f32, dims: Vec2) Vec2 {
@@ -42,25 +44,40 @@ fn translatePos(posX: f32, posY: f32, dims: Vec2) Vec2 {
 }
 
 export fn onRightClick() void {
-    triangles.items.len = temp_begin;
-    temp_triangle.len = 0;
+    if (temp_points.len != 0) {
+        triangles.items.len = temp_begin;
+        temp_points.len = 0;
 
-    const obj = wasm.out.slice(triangles.items);
-    ext.setTriangles(obj);
+        const obj = wasm.out.slice(triangles.items);
+        ext.setTriangles(obj);
+
+        return;
+    }
+
+    switch (temp_type) {
+        .triangle => {
+            temp_type = .line;
+            wasm.out.post(.info, "using line now", .{});
+        },
+        .line => {
+            temp_type = .triangle;
+            wasm.out.post(.info, "using triangle now", .{});
+        },
+    }
 }
 
 export fn onMove(posX: f32, posY: f32, width: f32, height: f32) void {
-    if (temp_triangle.len < 2) return;
+    if (temp_points.len < 2) return;
 
     const dims: Vec2 = .{ width, height };
     const pos = translatePos(posX, posY, dims);
 
-    const len = temp_triangle.len;
-    temp_triangle.slice()[(len - 2)..][0..2].* = pos;
+    const len = temp_points.len;
+    temp_points.slice()[(len - 2)..][0..2].* = pos;
 
-    if (temp_triangle.len < 4) return;
+    if (temp_points.len < 4) return;
 
-    const prev: Vec2 = temp_triangle.slice()[(len - 4)..][0..2].*;
+    const prev: Vec2 = temp_points.slice()[(len - 4)..][0..2].*;
 
     const vector = pos - prev;
     const rot90: Vec2 = .{ -vector[1], vector[0] };
@@ -85,31 +102,42 @@ export fn onMove(posX: f32, posY: f32, width: f32, height: f32) void {
     ext.setTriangles(obj);
 }
 
-fn onClick(posX: f32, posY: f32, width: f32, height: f32) !void {
-    const pos = translatePos(posX, posY, .{ width, height });
+fn onClick(pos: Vec2) !void {
+    switch (temp_type) {
+        .triangle => if (temp_points.len >= 6) {
+            triangles.items.len = temp_begin;
+            try triangles.appendSlice(temp_points.slice());
+            temp_begin = triangles.items.len;
+            temp_points.len = 0;
 
-    if (temp_triangle.len == temp_triangle.buffer.len) {
-        triangles.items.len = temp_begin;
-        try triangles.appendSlice(temp_triangle.slice());
-        temp_begin = triangles.items.len;
-        temp_triangle.len = 0;
+            const obj = wasm.out.slice(triangles.items);
+            ext.setTriangles(obj);
 
-        const obj = wasm.out.slice(triangles.items);
-        ext.setTriangles(obj);
+            wasm.out.post(.success, "new triangle!", .{});
 
-        wasm.out.post(.success, "new triangle!", .{});
+            return;
+        },
 
-        return;
+        .line => if (temp_points.len >= 4) {
+            temp_begin = triangles.items.len;
+            temp_points.len = 0;
+
+            const obj = wasm.out.slice(triangles.items);
+            ext.setTriangles(obj);
+            return;
+        },
+
+        // else => return,
     }
 
-    if (temp_triangle.len == 0) {
-        temp_triangle.buffer[0..2].* = pos;
-        temp_triangle.len += 2;
+    if (temp_points.len == 0) {
+        temp_points.buffer[0..2].* = pos;
+        temp_points.len += 2;
     }
 
-    const len = temp_triangle.len;
-    temp_triangle.buffer[len..][0..2].* = pos;
-    temp_triangle.len += 2;
+    const len = temp_points.len;
+    temp_points.buffer[len..][0..2].* = pos;
+    temp_points.len += 2;
 
     try triangles.appendSlice(&.{
         0, 0, 0, 0, 0, 0,
@@ -131,7 +159,7 @@ pub fn print(msg: wasm.Obj) !void {
 
 pub fn init() !void {
     wasm.initIfNecessary();
-    temp_triangle = try std.BoundedArray(f32, 6).init(0);
+    temp_points = try std.BoundedArray(f32, 6).init(0);
     triangles = ArrayList(f32).init(liu.Pages);
 
     // try triangles.appendSlice(&.{
