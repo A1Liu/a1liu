a9d0a118e2f6ffd2572ff6bd6b143a7a72d98651
Albert Liu <albertymliu@gmail.com>
2022-05-08 22:14:40 -0400

    meh

--- src/grapher.zig
+++ src/grapher.zig
@@ -42,10 +42,28 @@ fn translatePos(posX: f32, posY: f32, dims: Vec2) Vec2 {
     return .{ posX * 2 / dims[0] - 1, -(posY * 2 / dims[1] - 1) };
 }
 
+fn drawLineInto(buffer: *[12]f32, from: Vec2, to: Vec2, dims: Vec2) void {
+    const vector = to - from;
+    const rot90: Vec2 = .{ -vector[1], vector[0] };
+
+    const tangent_len = @sqrt(rot90[0] * rot90[0] + rot90[1] * rot90[1]);
+    const tangent = rot90 * @splat(2, 2 / tangent_len) / dims;
+
+    // first triangle, drawn clockwise
+    buffer[0..2].* = from + tangent;
+    buffer[2..4].* = to + tangent;
+    buffer[4..6].* = from - tangent;
+
+    // second triangle, drawn clockwise
+    buffer[6..8].* = from - tangent;
+    buffer[8..10].* = to + tangent;
+    buffer[10..12].* = to - tangent;
+}
+
 export fn onRightClick() void {
-    if (temp_points.len != 0) {
+    if (temp_points.len > 2) {
         triangles.items.len = temp_begin;
-        temp_points.len = 0;
+        temp_points.len = 2;
 
         const obj = wasm.out.slice(triangles.items);
         ext.setTriangles(obj);
@@ -66,72 +84,72 @@ export fn onRightClick() void {
 }
 
 export fn onMove(posX: f32, posY: f32, width: f32, height: f32) void {
-    if (temp_points.len < 2) return;
-
     const dims: Vec2 = .{ width, height };
     const pos = translatePos(posX, posY, dims);
 
     const len = temp_points.len;
+
+    std.debug.assert(len >= 2);
+
     temp_points.slice()[(len - 2)..][0..2].* = pos;
 
     if (temp_points.len < 4) return;
 
     const prev: Vec2 = temp_points.slice()[(len - 4)..][0..2].*;
 
-    const vector = pos - prev;
-    const rot90: Vec2 = .{ -vector[1], vector[0] };
-
-    const tangent_len = @sqrt(rot90[0] * rot90[0] + rot90[1] * rot90[1]);
-    const tangent = rot90 * @splat(2, 2 / tangent_len) / dims;
-
     const data_begin = temp_begin + ((len - 4) / 2 * 12);
     const data = triangles.items[data_begin..];
+    drawLineInto(data[0..12], prev, pos, dims);
 
-    // first triangle, drawn clockwise
-    data[0..2].* = prev + tangent;
-    data[2..4].* = pos + tangent;
-    data[4..6].* = prev - tangent;
+    temp_type: {
+        if (temp_type == .triangle) {
+            if (temp_points.len < 6) break :temp_type;
 
-    // second triangle, drawn clockwise
-    data[6..8].* = prev - tangent;
-    data[8..10].* = pos + tangent;
-    data[10..12].* = pos - tangent;
+            const first = temp_points.slice()[0..2].*;
+            const data2 = triangles.items[(data_begin + 12)..];
+            drawLineInto(data2[0..12], first, pos, dims);
+        }
+    }
 
     const obj = wasm.out.slice(triangles.items);
     ext.setTriangles(obj);
 }
 
 pub fn onClick(pos: Vec2) !void {
+    std.debug.assert(temp_points.len >= 2);
+    temp_points.slice()[(temp_points.len - 2)..][0..2].* = pos;
+
     switch (temp_type) {
-        .triangle => if (temp_points.len >= 6) {
-            triangles.items.len = temp_begin;
-            try triangles.appendSlice(temp_points.slice());
-            temp_begin = triangles.items.len;
-            temp_points.len = 0;
+        .triangle => {
+            if (temp_points.len >= 6) {
+                triangles.items.len = temp_begin;
+                try triangles.appendSlice(temp_points.slice());
+                temp_begin = triangles.items.len;
+                temp_points.len = 2;
 
-            const obj = wasm.out.slice(triangles.items);
-            ext.setTriangles(obj);
+                const obj = wasm.out.slice(triangles.items);
+                ext.setTriangles(obj);
 
-            wasm.out.post(.success, "new triangle!", .{});
+                wasm.out.post(.success, "new triangle!", .{});
 
-            return;
+                return;
+            }
+
+            if (temp_points.len == 4) {
+                try triangles.appendSlice(&.{
+                    0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                });
+            }
         },
 
         .line => if (temp_points.len >= 4) {
             temp_begin = triangles.items.len;
-            temp_points.len = 0;
-
-            const obj = wasm.out.slice(triangles.items);
-            ext.setTriangles(obj);
+            temp_points.len = 2;
             return;
         },
 
         // else => return,
-    }
-
-    if (temp_points.len == 0) {
-        temp_points.buffer[0..2].* = pos;
-        temp_points.len += 2;
     }
 
     const len = temp_points.len;
@@ -158,8 +176,10 @@ pub fn print(msg: wasm.Obj) !void {
 
 pub fn init() !void {
     wasm.initIfNecessary();
-    temp_points = try std.BoundedArray(f32, 6).init(0);
+    temp_points = try std.BoundedArray(f32, 6).init(2);
     triangles = ArrayList(f32).init(liu.Pages);
+
+    temp_points.buffer[0..2].* = .{ 0, 0 };
 
     std.log.info("WASM initialized!", .{});
 }
