f2e4ed72719d7156f623cb1f3e1a7588a1e855aa
Albert Liu <albertymliu@gmail.com>
2022-05-27 16:22:56 -0700

    first bit of pokemario

---
+++ pokemario/sky/index.ts
@@ -0,0 +1,35 @@
+import { Sprite, Vector2 } from '../sprite';
+import { Game } from '../game';
+import skyJpg from './sky.jpg';
+
+export class SkyBackground extends Sprite {
+    velocity: Vector2 = {
+        x: -5,
+        y: 0,
+    };
+
+    constructor() {
+        super({
+            x: 0,
+            y: 0,
+        }, {
+            width: 0,
+            height: 0,
+        }, skyJpg.src)
+        this.image = new Image()
+        this.image.src = skyJpg.src
+    }
+
+    tick(delta: number, game: Game) {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+
+    render(game: Game, ctx: CanvasRenderingContext2D) {
+        this.size = {
+            width: game.width,
+            height: game.height,
+        }
+        super.render(game, ctx)
+    }
+}
