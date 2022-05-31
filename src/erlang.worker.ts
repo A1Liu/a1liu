5623f61b5a62436873a27400ddf32abb2106881c
Albert Liu <albertymliu@gmail.com>
2022-05-30 19:09:36 -0700

    meh

--- src/erlang.worker.ts
+++ src/erlang.worker.ts
@@ -1,18 +1,18 @@
-import * as GL from "src/webgl";
-import type { WebGl } from "src/webgl";
-import * as wasm from "src/wasm";
+import * as GL from 'src/webgl';
+import type { WebGl } from 'src/webgl';
+import * as wasm from 'src/wasm';
 
 export type Number2 = [number, number];
 export type Number3 = [number, number, number];
 export type Number4 = [number, number, number, number];
 
 export type Message =
-  | { kind: "resize"; data: Number2 }
-  | { kind: "mousemove"; data: Number2 }
-  | { kind: "leftclick"; data: Number2 }
-  | { kind: "rightclick"; data: Number2 }
-  | { kind: "keydown"; data: number }
-  | { kind: "canvas"; offscreen: any };
+  | { kind: 'resize'; data: Number2 }
+  | { kind: 'mousemove'; data: Number2 }
+  | { kind: 'leftclick'; data: Number2 }
+  | { kind: 'rightclick'; data: Number2 }
+  | { kind: 'keydown'; data: number }
+  | { kind: 'canvas'; offscreen: any };
 
 let resolve: null | ((msg: Message[]) => void) = null;
 const messages: Message[] = [];
@@ -37,7 +37,7 @@ const waitForMessage = (): Promise<Message[]> => {
 };
 
 export type OutMessage =
-  | { kind: "initDone"; data?: void }
+  | { kind: 'initDone'; data?: void }
   | { kind: string; data: any };
 
 interface ErlangGlState {
@@ -62,15 +62,15 @@ const glState: ErlangGlState = {
 };
 
 const initGl = async (canvas: any): Promise<ErlangGl | null> => {
-  const ctx: WebGl = canvas?.getContext("webgl2", {
+  const ctx: WebGl = canvas?.getContext('webgl2', {
     preserveDrawingBuffer: true,
   });
 
   if (!ctx) return null;
 
   const [vertSrc, fragSrc] = await Promise.all([
-    fetch("/apps/painter.vert").then((r) => r.text()),
-    fetch("/apps/painter.frag").then((r) => r.text()),
+    fetch('/apps/painter.vert').then((r) => r.text()),
+    fetch('/apps/painter.frag').then((r) => r.text()),
   ]);
 
   const vertexShader = GL.createShader(ctx, ctx.VERTEX_SHADER, vertSrc);
@@ -92,8 +92,8 @@ const initGl = async (canvas: any): Promise<ErlangGl | null> => {
   const posLocation = 0;
   const colorLocation = 1;
 
-  ctx.bindAttribLocation(program, posLocation, "pos");
-  ctx.bindAttribLocation(program, colorLocation, "color");
+  ctx.bindAttribLocation(program, posLocation, 'pos');
+  ctx.bindAttribLocation(program, colorLocation, 'color');
 
   ctx.bindVertexArray(vao);
 
@@ -209,29 +209,29 @@ const updateState = (
 
 const handleMessage = (wasmRef: wasm.Ref, msg: Message) => {
   switch (msg.kind) {
-    case "mousemove": {
+    case 'mousemove': {
       const [x, y] = msg.data;
       wasmRef.abi.onMove(x, y);
       break;
     }
 
-    case "leftclick": {
+    case 'leftclick': {
       const [x, y] = msg.data;
       wasmRef.abi.onClick(x, y);
       break;
     }
 
-    case "rightclick":
+    case 'rightclick':
       const [x, y] = msg.data;
       wasmRef.abi.onRightClick(x, y);
       break;
 
-    case "keydown":
+    case 'keydown':
       const data = `${msg.data}`;
       wasmRef.abi.onKey(msg.data);
       break;
 
-    case "resize": {
+    case 'resize': {
       const [width, height] = msg.data;
       resize(wasmRef, width, height);
 
@@ -254,7 +254,7 @@ const main = async (wasmRef: wasm.Ref) => {
 };
 
 const init = async () => {
-  const wasmRef = await wasm.fetchWasm("/apps/erlang.wasm", {
+  const wasmRef = await wasm.fetchWasm('/apps/erlang.wasm', {
     postMessage: (kind: string, data: any) => postMessage({ kind, data }),
     raw: (wasmRef: wasm.Ref) => ({}),
     imports: {
@@ -271,7 +271,7 @@ const init = async () => {
 
     captured.forEach((msg) => {
       switch (msg.kind) {
-        case "canvas":
+        case 'canvas':
           offscreen = msg.offscreen;
           break;
 
@@ -286,13 +286,13 @@ const init = async () => {
     const ggl = await initGl(offscreen);
 
     if (!ggl) {
-      postMessage({ kind: "error", data: "WebGL2 not supported!" });
+      postMessage({ kind: 'error', data: 'WebGL2 not supported!' });
       return;
     }
 
     gglRef.current = ggl;
-    postMessage({ kind: "success", data: "WebGL2 context initialized!" });
-    postMessage({ kind: "initDone" });
+    postMessage({ kind: 'success', data: 'WebGL2 context initialized!' });
+    postMessage({ kind: 'initDone' });
 
     wasmRef.abi.initialRender();
     break;
