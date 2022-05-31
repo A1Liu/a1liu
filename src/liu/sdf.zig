240386c0f860f126f37cd0ce64862e2c2e6f31a0
Albert Liu <albertymliu@gmail.com>
2022-05-30 22:02:31 -0700

    lol

--- src/liu/sdf.zig
+++ src/liu/sdf.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const liu = @import("./lib.zig");
 
+// https://learnopengl.com/In-Practice/Text-Rendering
 // https://steamcdn-a.akamaihd.net/apps/valve/2007/SIGGRAPH2007_AlphaTestedMagnification.pdf
 // https://github.com/mapbox/sdf-glyph-foundry/blob/master/include/mapbox/glyph_foundry_impl.hpp
 // https://blog.mapbox.com/drawing-text-with-signed-distance-fields-in-mapbox-gl-b0933af6f817
