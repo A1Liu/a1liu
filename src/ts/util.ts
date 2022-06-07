0e5a1d38175ad0f3aa4a733b16b06f4f3487470e
Albert Liu <albertymliu@gmail.com>
2022-06-06 21:15:09 -0700

    meh

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -31,14 +31,15 @@ export async function get(urlString: string, query: any): Promise<any> {
 
 export class WorkerCtx<T> {
   private messages: T[] = [];
-  private resolve: ((t: T) => void) | null = null;
+  private resolve?: (t: T) => void;
 
   onmessageCallback(): (event: MessageEvent<T>) => void {
     return (event) => {
       this.messages.push(event.data);
+
       if (this.resolve) {
         this.resolve(this.messages.splice(0, this.messages.length));
-        this.resolve = null;
+        this.resolve = undefined;
       }
     };
   }
