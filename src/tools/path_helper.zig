e044558941b8db8849797f45239e4ddb5f2de785
Albert Liu <albertymliu@gmail.com>
2022-06-16 00:02:58 -0700

    meh

--- src/tools/path_helper.zig
+++ src/tools/path_helper.zig
@@ -1,1 +1,7 @@
-pub fn main() void {}
+const std = @import("std");
+const builtin = @import("builtin");
+const liu = @import("liu");
+
+pub fn main() !void {
+    std.debug.print("Hello world!\n", .{});
+}
