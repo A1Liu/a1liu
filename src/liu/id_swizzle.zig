ba04e725fd39dcd2595b152ec838175282424e9d
Albert Liu <albertymliu@gmail.com>
2022-07-01 15:13:24 -0700

    meh

--- src/liu/id_swizzle.zig
+++ src/liu/id_swizzle.zig
@@ -1,19 +1,48 @@
 const std = @import("std");
 
-const ID_MASK: u32 = 0b10100110_01101010_01001010_10101010;
-const ID_ADD: u32 = 2740160927;
+fn idMask(comptime T: type) T {
+    return switch (T) {
+        u32 => 0b10100110_01101010_01001010_10101010,
 
-// These two numbers are multiplicative inverses mod 2^32
-const ID_MUL_TO: u32 = 0x01000193;
-const ID_MUL_FROM: u32 = 0x359c449b;
+        else => @compileError("the type '" ++ @typeName(T) ++ "' doesn't work as an ID type"),
+    };
+}
+
+fn idAdd(comptime T: type) T {
+    return switch (T) {
+        u32 => 2740160927,
+
+        else => @compileError("the type '" ++ @typeName(T) ++ "' doesn't work as an ID type"),
+    };
+}
+
+fn IdMul(comptime T: type) type {
+    return struct {
+        to: T,
+        from: T,
+    };
+}
+
+// These two numbers are multiplicative inverses mod T
+fn idMul(comptime T: type) IdMul(T) {
+    return switch (T) {
+        u32 => .{ .to = 0x01000193, .from = 0x359c449b },
+
+        else => @compileError("the type '" ++ @typeName(T) ++ "' doesn't work as an ID type"),
+    };
+}
 
 // const ID_ROTATE_BITS: u32 = 16;
 // let s2 = s1.swap_bytes();
 // let s5 = s4.rotate_left(ID_ROTATE_BITS);
 
-pub fn Id(comptime T: type) type {
+pub fn SwizzledId(comptime T: type) type {
     return enum(T) {
         _,
+
+        const ADD = idAdd(T);
+        const MASK = idMask(T);
+        const MUL = idMul(T);
 
         const Self = @This();
 
@@ -26,17 +55,17 @@ pub fn Id(comptime T: type) type {
         }
 
         pub inline fn fromSwizzle(val: T) Self {
-            const s3 = val +% ID_ADD;
-            const s2 = s3 *% ID_MUL_FROM;
-            const s1 = s2 ^ ID_MASK;
+            const s3 = val +% ADD;
+            const s2 = s3 *% MUL.from;
+            const s1 = s2 ^ MASK;
 
             return @intToEnum(Self, s1);
         }
 
         pub fn swizzle(self: Self) T {
-            const s1 = @enumToInt(self) ^ ID_MASK;
-            const s2 = s1 *% ID_MUL_TO;
-            const s3 = s2 -% ID_ADD;
+            const s1 = @enumToInt(self) ^ MASK;
+            const s2 = s1 *% MUL.to;
+            const s3 = s2 -% ADD;
 
             return s3;
         }
@@ -44,14 +73,16 @@ pub fn Id(comptime T: type) type {
 }
 
 test "Id swizzle: basic" {
-    try std.testing.expectEqual(ID_MUL_TO *% ID_MUL_FROM, 1);
+    const Id = SwizzledId(u32);
 
-    const tests = [_]u32{ ID_MASK, ID_ADD, ID_MUL_TO, ID_MUL_FROM };
+    try std.testing.expectEqual(Id.MUL.to *% Id.MUL.from, 1);
+
+    const tests = [_]u32{ Id.MASK, Id.ADD, Id.MUL.to, Id.MUL.from };
 
     {
         var id: u32 = 0;
         while (id < 100) : (id += 1) {
-            const value = Id(u32).fromSwizzle(id);
+            const value = Id.fromSwizzle(id);
             const out_id = value.swizzle();
 
             // println!("{} -> {}", id, value);
@@ -63,7 +94,7 @@ test "Id swizzle: basic" {
     }
 
     for (tests) |id| {
-        const value = Id(u32).fromSwizzle(id);
+        const value = Id.fromSwizzle(id);
         const out_id = value.swizzle();
 
         // println!("{} -> {}", id, value);
@@ -73,7 +104,7 @@ test "Id swizzle: basic" {
 
     var value: u32 = 0;
     while (value < 100) : (value += 100) {
-        const id = Id(u32).fromSwizzle(value);
+        const id = Id.fromSwizzle(value);
         const out_value = id.swizzle();
 
         // println!("{} -> {}", id, value);
