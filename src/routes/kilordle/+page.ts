56e4fdcd62e1f334b585086d9c812dd0a6d48f1b
Albert Liu <albertymliu@gmail.com>
2023-02-05 00:17:51 -0500

    'meh

--- src/routes/kilordle/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from "./$types";
-import { fetchAsset } from "@lib/ts/wasm";
-
-export const load: PageLoad = async ({ fetch, params }) => {
-  const data = fetchAsset("/kilordle/data.rtf");
-
-  return { data };
-};
