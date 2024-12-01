0c1d7acd414e9fe7f3d35fa241c3d64cc7ffd896
Albert Liu <albertymliu@gmail.com>
2024-11-30 23:44:20 -0500

    did some stuff that hopefully fixes some weird parser error

--- config/programs/neovim/lua/plugins.lua
+++ config/programs/neovim/lua/plugins.lua
@@ -22,7 +22,21 @@ local PluginsSpec = {}
 
 table.insert(
   PluginsSpec,
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+          ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "zig",
+            "vimdoc",
+          },
+        })
+    end,
+  }
   )
 
 table.insert(PluginsSpec, {"preservim/nerdtree"})
@@ -44,11 +58,11 @@ vim.api.nvim_set_keymap('n', '<C-B>', '', {
     end
   })
 
-[[
 local id = vim.api.nvim_create_augroup("NerdTree", {
     clear = true
   })
 
+--[[
 vim.api.nvim_create_autocmd({"BufEnter"}, {
     pattern = {"nerdtree"},
     group = id,
@@ -56,24 +70,24 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {
       vim.b.ALIU_NERDTREE_BUFFER = true
 
       -- vim.api.nvim_buf_set_keymap('n', '<C-J>', '4gj', {
-      --     noremap = true,
-      --   })
+          --     noremap = true,
+        --   })
 
       -- vim.api.nvim_buf_set_keymap('n', '<C-J>', '4gj', {
-      --     noremap = true,
-      --   })
+          --     noremap = true,
+        --   })
       -- vim.api.nvim_buf_set_keymap('n', '<C-K>', '4gk', {
-      --     noremap = true,
-      --   })
+          --     noremap = true,
+        --   })
       -- vim.api.nvim_buf_set_keymap('v', '<C-J>', '4gj', {
-      --     noremap = true,
-      --   })
+          --     noremap = true,
+        --   })
       -- vim.api.nvim_buf_set_keymap('v', '<C-K>', '4gk', {
-      --     noremap = true,
-      --   })
+          --     noremap = true,
+        --   })
     end
   })
-]]
+]]--
 
 lazy_plugins.setup({
     spec = PluginsSpec,
