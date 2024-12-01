cf3998514fda17b7d0d5d14eedeabc62458674f7
Albert Liu <albertymliu@gmail.com>
2024-12-01 00:38:15 -0500

    tried some more but things are broken now...

--- config/programs/neovim/lua/plugins.lua
+++ config/programs/neovim/lua/plugins.lua
@@ -5,10 +5,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-        { out, "WarningMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
@@ -20,24 +20,21 @@ local lazy_plugins = require("lazy")
 
 local PluginsSpec = {}
 
-table.insert(
-  PluginsSpec,
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-          ensure_installed = {
-            "c",
-            "lua",
-            "vim",
-            "zig",
-            "vimdoc",
-          },
-        })
-    end,
-  }
-  )
+table.insert( PluginsSpec, {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "zig",
+        "vimdoc",
+      },
+    })
+  end,
+})
 
 table.insert(PluginsSpec, {"preservim/nerdtree"})
 
@@ -45,56 +42,25 @@ vim.g.NERDTreeMapJumpNextSibling = ""
 vim.g.NERDTreeMapJumpPrevSibling = ""
 
 vim.api.nvim_set_keymap('n', '<C-B>', '', {
-    noremap = true,
-    callback = function()
+  noremap = true,
+  callback = function()
 
-      if string.find(vim.bo.filetype, "nerdtree") then
-        vim.cmd('NERDTreeClose')
-      elseif vim.fn.expand("%") == "" then
-        vim.cmd('NERDTreeFocus')
-      else
-        vim.cmd('NERDTreeFind')
-      end
+    if string.find(vim.bo.filetype, "nerdtree") then
+      vim.cmd('NERDTreeClose')
+    elseif vim.fn.expand("%") == "" then
+      vim.cmd('NERDTreeFocus')
+    else
+      vim.cmd('NERDTreeFind')
     end
-  })
-
-local id = vim.api.nvim_create_augroup("NerdTree", {
-    clear = true
-  })
-
---[[
-vim.api.nvim_create_autocmd({"BufEnter"}, {
-    pattern = {"nerdtree"},
-    group = id,
-    callback = function(ev)
-      vim.b.ALIU_NERDTREE_BUFFER = true
-
-      -- vim.api.nvim_buf_set_keymap('n', '<C-J>', '4gj', {
-          --     noremap = true,
-        --   })
-
-      -- vim.api.nvim_buf_set_keymap('n', '<C-J>', '4gj', {
-          --     noremap = true,
-        --   })
-      -- vim.api.nvim_buf_set_keymap('n', '<C-K>', '4gk', {
-          --     noremap = true,
-        --   })
-      -- vim.api.nvim_buf_set_keymap('v', '<C-J>', '4gj', {
-          --     noremap = true,
-        --   })
-      -- vim.api.nvim_buf_set_keymap('v', '<C-K>', '4gk', {
-          --     noremap = true,
-        --   })
-    end
-  })
-]]--
+  end
+})
 
 lazy_plugins.setup({
-    spec = PluginsSpec,
+  spec = PluginsSpec,
 
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "wildcharm" } },
-    -- automatically check for plugin updates
-    -- checker = { enabled = true, notify = false },
-  })
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "wildcharm" } },
+  -- automatically check for plugin updates
+  -- checker = { enabled = true, notify = false },
+})
