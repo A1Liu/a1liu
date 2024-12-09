e9cd7dc59694ce4c4eea50347e561bf64c05f3c8
Albert Liu <albertymliu@gmail.com>
2024-12-09 15:27:47 -0500

    setting up config params

--- config/programs/neovim/lua/plugins.lua
+++ config/programs/neovim/lua/plugins.lua
@@ -5,22 +5,76 @@ local Plug = Util.import("vim-plug")
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
-Plug('nvim-tree/nvim-tree.lua')
-Plug('nvim-tree/nvim-web-devicons') -- optional
+Plug('nvim-tree/nvim-tree.lua', {
+  config = function()
+    local tree = require("nvim-tree")
+    local api = require("nvim-tree.api")
+    vim.api.nvim_set_keymap('n', '<C-B>', '', {
+      noremap = true,
+      callback = function()
+        if vim.fn.expand("%") == "" then
+          vim.cmd('NvimTreeFocus')
+        else
+          vim.cmd('NvimTreeFindFile')
+        end
+      end
+    })
 
-vim.g.NERDTreeMapJumpNextSibling = ""
-vim.g.NERDTreeMapJumpPrevSibling = ""
-
-vim.api.nvim_set_keymap('n', '<C-B>', '', {
-  noremap = true,
-  callback = function()
-    if string.find(vim.bo.filetype, "nerdtree") then
-      vim.cmd('NvimTreeToggle')
-    elseif vim.fn.expand("%") == "" then
-      vim.cmd('NvimTreeFocus')
-    else
-      vim.cmd('NvimTreeFindFile')
+    local on_attach = function(bufnr)
+      vim.keymap.set("n", "C", api.tree.change_root_to_node, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "<CR>", api.node.open.no_window_picker, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "D", api.fs.trash, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "a", api.fs.create, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "R", api.tree.reload, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
+      vim.keymap.set("n", "<C-B>", api.tree.close, {
+        buffer = bufnr, noremap = true, silent = true, nowait = true
+      })
     end
+
+    tree.setup({
+      on_attach = on_attach,
+      renderer = {
+        icons = {
+          show = {
+            git = true,
+            file = false,
+            folder = false,
+            folder_arrow = true,
+          },
+          glyphs = {
+            folder = {
+              arrow_closed = "⏵",
+              arrow_open = "⏷",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "⌥",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "⊖",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+    })
   end
 })
 
@@ -28,11 +82,49 @@ Plug("nvim-treesitter/nvim-treesitter", {
   run = function()
     vim.cmd('TSUpdate')
   end,  -- We recommend updating the parsers on update
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      highlight = {
+        enable = true
+      },
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+      },
+    }
+  end,
 })
 
+--[[
+Likely installs:
+- typescript-language-server
+- lua-language-server
+- prettier
+]]--
 Plug("williamboman/mason.nvim")
 Plug("williamboman/mason-lspconfig.nvim")
-Plug("neovim/nvim-lspconfig")
+Plug("neovim/nvim-lspconfig", {
+  config = function()
+    -- Setup order matters here
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+
+    local lspconfig = require('lspconfig')
+
+    lspconfig.ts_ls.setup {
+      on_init = function(client, _)
+        client.server_capabilities.semanticTokensProvider = nil  -- turn off semantic tokens
+      end,
+      filetypes = {
+        "javascript",
+        "typescript",
+        "typescriptreact",
+        "javascriptreact",
+      },
+    }
+  end
+})
 
 vim.keymap.set('v', '<C-F>', '', { noremap = true })
 vim.keymap.set('n', '<C-F>', vim.lsp.buf.code_action, {
@@ -118,84 +210,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 Plug.ends()
 
-require("nvim-tree").setup({
- renderer = {
-    icons = {
-      show = {
-        git = true,
-        file = false,
-        folder = false,
-        folder_arrow = true,
-      },
-      glyphs = {
-        folder = {
-          arrow_closed = "⏵",
-          arrow_open = "⏷",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "⌥",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "⊖",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-  on_attach = function(bufnr)
-    local api = require("nvim-tree.api")
-    vim.keymap.set("n", "C", api.tree.change_root_to_node, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "<CR>", api.node.open.no_window_picker, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "D", api.fs.trash, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "a", api.fs.create, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-    vim.keymap.set("n", "R", api.tree.reload, {
-      buffer = bufnr, noremap = true, silent = true, nowait = true
-    })
-  end
-})
-
-require("mason").setup()
--- require('lspconfig').pyright.setup{}
-require('lspconfig').ts_ls.setup {
-  on_init = function(client, _)
-    client.server_capabilities.semanticTokensProvider = nil  -- turn off semantic tokens
-  end,
-  filetypes = {
-    "javascript",
-    "typescript",
-    "typescriptreact",
-    "javascriptreact",
-  },
-}
-
-
---[[
-require('nvim-treesitter.configs').setup {
-  highlight = {
-    enable = true
-  },
-  ensure_installed = {
-    "c",
-    "lua",
-    "vim",
-    "zig",
-    "vimdoc",
-  },
-}
-]]--
