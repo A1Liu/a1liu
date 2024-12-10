929585d7b3752c3bc7a826fad3f008b50a7f8a94
Albert Liu <albertymliu@gmail.com>
2024-12-10 12:16:14 -0500

    moved more into plugin setup

--- config/programs/neovim/lua/plugins.lua
+++ config/programs/neovim/lua/plugins.lua
@@ -127,90 +127,90 @@ Plug("neovim/nvim-lspconfig", {
         "javascriptreact",
       },
     }
-  end
-})
 
-vim.keymap.set('v', '<C-F>', '', { noremap = true })
-vim.keymap.set('n', '<C-F>', vim.lsp.buf.code_action, {
-  noremap = true,
-})
+    vim.keymap.set('v', '<C-F>', '', { noremap = true })
+    vim.keymap.set('n', '<C-F>', vim.lsp.buf.code_action, {
+      noremap = true,
+    })
 
--- Forgot how to do this, had to use vim tutorial to help:
--- https://vim.fandom.com/wiki/Improve_completion_popup_menu
---
--- note that we need to use VimEnter here because otherwise vim-rsi
--- overwrites <C-F> .
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function(args)
-    vim.keymap.set('i', '<C-F>', function()
-      print("Hello")
+    -- Forgot how to do this, had to use vim tutorial to help:
+    -- https://vim.fandom.com/wiki/Improve_completion_popup_menu
+    --
+    -- note that we need to use VimEnter here because otherwise vim-rsi
+    -- overwrites <C-F> .
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function(args)
+        vim.keymap.set('i', '<C-F>', function()
+          print("Hello")
+          if vim.fn.pumvisible() == 1 then
+            -- Confirm in omnifunc
+            return "<C-Y>"
+          else
+            -- Open in omnifunc
+            return "<C-X><C-O>"
+          end
+        end, { noremap = true, expr = true })
+      end,
+    })
+
+    -- Using <C-J> and <C-K> for navigating the pop-up menu
+    -- inoremap <C-N><C-O> <C-N>
+    -- inoremap <C-N><C-O> <C-X><C-O>
+    -- inoremap <C-N> <Nop>
+    -- inoremap <C-N><C-T> <C-N>
+    -- inoremap <expr> <C-D> pumvisible() ? "\<C-N>\<C-N>\<C-N>\<C-N>\<C-N>" : "\<C-D>"
+    -- inoremap <expr> <C-U> pumvisible() ? "\<C-P>\<C-P>\<C-P>\<C-P>\<C-P>" : "\<C-U>"
+    vim.keymap.set('i', '<C-J>', function()
       if vim.fn.pumvisible() == 1 then
-        -- Confirm in omnifunc
-        return "<C-Y>"
+        return "<C-N>"
       else
-        -- Open in omnifunc
-        return "<C-X><C-O>"
+        return "<C-J>"
       end
     end, { noremap = true, expr = true })
-  end,
-})
 
--- Using <C-J> and <C-K> for navigating the pop-up menu
--- inoremap <C-N><C-O> <C-N>
--- inoremap <C-N><C-O> <C-X><C-O>
--- inoremap <C-N> <Nop>
--- inoremap <C-N><C-T> <C-N>
--- inoremap <expr> <C-D> pumvisible() ? "\<C-N>\<C-N>\<C-N>\<C-N>\<C-N>" : "\<C-D>"
--- inoremap <expr> <C-U> pumvisible() ? "\<C-P>\<C-P>\<C-P>\<C-P>\<C-P>" : "\<C-U>"
-vim.keymap.set('i', '<C-J>', function()
-  if vim.fn.pumvisible() == 1 then
-    return "<C-N>"
-  else
-    return "<C-J>"
-  end
-end, { noremap = true, expr = true })
+    vim.keymap.set('i', '<C-K>', function()
+      if vim.fn.pumvisible() == 1 then
+        return "<C-P>"
+      else
+        return "<C-K>"
+      end
+    end, { noremap = true, expr = true })
 
-vim.keymap.set('i', '<C-K>', function()
-  if vim.fn.pumvisible() == 1 then
-    return "<C-P>"
-  else
-    return "<C-K>"
-  end
-end, { noremap = true, expr = true })
+    vim.keymap.set('n', '<Leader>b', vim.lsp.buf.implementation, {
+      noremap = true,
+    })
+    vim.keymap.set('n', '<C-E>', vim.lsp.buf.hover, {
+      noremap = true,
+    })
+    -- inoremap <silent><expr> <Tab> pumvisible() ? "\<C-n>" : "\<C-x>\<C-o>"
 
-vim.keymap.set('n', '<Leader>b', vim.lsp.buf.implementation, {
-  noremap = true,
-})
-vim.keymap.set('n', '<C-E>', vim.lsp.buf.hover, {
-  noremap = true,
-})
--- inoremap <silent><expr> <Tab> pumvisible() ? "\<C-n>" : "\<C-x>\<C-o>"
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        vim.print("LSP Attached!")
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    vim.print("LSP Attached!")
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        -- if client.supports_method('textDocument/implementation') then
+        -- end
 
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    -- if client.supports_method('textDocument/implementation') then
-    -- end
-
-    --[[
+        --[[
     if client.supports_method('textDocument/completion') then
        -- Enable auto-completion
        vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
     end
     ]] --
 
-    if client.supports_method('textDocument/formatting') then
-      -- Format the current buffer on save
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-        end,
-      })
-    end
-  end,
+        if client.supports_method('textDocument/formatting') then
+          -- Format the current buffer on save
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+            end,
+          })
+        end
+      end,
+    })
+  end
 })
 
 -- Plug.begin()
