d94f502e22d9a536ec314c0d275a624757ddd3cb
Albert Liu <albertymliu@gmail.com>
2024-12-25 17:58:33 -0500

    adding fingerprint auth

--- config/programs/nix/configuration.nix
+++ config/programs/nix/configuration.nix
@@ -121,6 +121,11 @@
   #  wget
   ];
 
+  services.fprintd.enable = true;
+
+  security.pam.services.swaylock = {};
+  security.pam.services.swaylock.fprintAuth = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
