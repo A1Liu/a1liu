4faa211158ae87f0c3ddc112d54c9fafb7b27cfc
albert-h3 <albert.liu@horizon3.ai>
2025-01-05 16:58:08 -0500

    Add nix-darwin configs (#55)
    Co-authored-by: Albert Liu <albertymliu@gmail.com>

--- macos/flake.nix
+++ macos/flake.nix
@@ -12,8 +12,12 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-        pkgs.vim
+      environment.systemPackages = with pkgs; [
+        vim
+        neovim
+        ripgrep
+        tmux
+        neovide
       ];
 
       # Necessary for using flakes on this system.
@@ -31,6 +35,20 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+      # set some OSX preferences that I always end up hunting down and changing.
+      system.defaults = {
+        # minimal dock
+        dock = {
+          show-recents = false;
+        };
+        # a finder that tells me what I want to know and lets me work
+        finder = {
+          AppleShowAllExtensions = true;
+          ShowPathbar = true;
+          FXEnableExtensionChangeWarning = false;
+        };
+      };
+
     };
   in
   {
