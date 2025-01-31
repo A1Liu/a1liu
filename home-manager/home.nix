f7ca19dd9c99ea2afd6509a44c0600d97e6fde0b
Albert Liu <albertymliu@gmail.com>
2025-01-30 22:22:35 -0500

    idk

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,7 +55,17 @@ in
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  home.file = let
+    createShellEntrypoint = isInteractive: ''
+      #!/bin/sh
+
+      export CFG_DIR="{config.project_dir}"
+      CUR_SHELL="$(basename "$0" 2>/dev/null || echo "$0" | tr -d "-")"
+      IS_INTERACTIVE_SHELL=${isInteractive}
+
+      . "{config.project_dir}/programs/shells/dispatch"
+    '';
+  in {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -66,6 +76,7 @@ in
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".nix-zshrc".text = createShellEntrypoint "true";
   };
 
   # Home Manager can also manage your environment variables through
