7fae266d4b6dd49d0c8dcb8744712072a5660de5
Albert Liu <albertymliu@gmail.com>
2025-02-01 00:43:02 -0500

    moved files

--- vim/init.lua
+++ vim/init.lua
@@ -1,10 +1,10 @@
 local home = vim.fs.dirname(vim.fn.resolve(vim.fn.expand('<sfile>:p')))
-local cfg_dir = vim.fs.dirname(vim.fs.dirname(home))
+local cfg_dir = vim.fs.dirname(home)
 
 Config = {
   home = home,
   cfg_dir = cfg_dir,
-  vim_dir = vim.fs.joinpath(cfg_dir, "programs", "vim"),
+  vim_dir = vim.fs.joinpath(cfg_dir, "vim"),
 }
 
 -- Basic configs
